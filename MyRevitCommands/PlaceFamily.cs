2a71a4ae85971d0bbc1b4c0547adbebede1d21ac
frankleyyago <frankleyyago@outlook.com>
2023-09-20 17:50:54 -0300

    feat: place a family into a document

---
+++ MyRevitCommands/PlaceFamily.cs
@@ -0,0 +1,63 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRevitCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class PlaceFamily : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get document
+            Document doc = uidoc.Document;
+
+            //Get family symbol
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            IList<Element> symbols = collector.OfClass(typeof(FamilySymbol)).WhereElementIsElementType().ToElements();
+
+            FamilySymbol symbol = null;
+            foreach (Element ele in symbols)
+            {
+                if(ele.Name == "60\" x 30\"")
+                {
+                    symbol = ele as FamilySymbol;
+                    break;
+                }
+            }
+
+            //Create a transaction
+            try
+            {
+                using (Transaction trans = new Transaction(doc, "Place family"))
+                {
+                    trans.Start();
+
+                    if (!symbol.IsActive)
+                    {
+                        symbol.Activate();
+                    }
+
+                    doc.Create.NewFamilyInstance(new XYZ(0, 0, 0), symbol, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+
+                    trans.Commit();
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
