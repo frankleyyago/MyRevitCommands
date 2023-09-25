acae3f0aab0139f32c0f6474a360444086a1af06
frankleyyago <frankleyyago@outlook.com>
2023-09-25 17:35:41 -0300

    feat: move and rotate element

---
+++ MyRevitCommands/EditElement.cs
@@ -0,0 +1,65 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRevitCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class EditElement : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get document
+            Document doc = uidoc.Document;
+
+            try
+            {
+                //Pick object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                //Display element id
+                if (pickedObj != null)
+                {
+                    //Retrieve element
+                    ElementId eleId = pickedObj.ElementId;
+                    Element ele = doc.GetElement(eleId);
+
+                    using (Transaction trans = new Transaction(doc, "Edit element"))
+                    {
+                        trans.Start();
+
+                        //Move element
+                        XYZ moveVec = new XYZ(3, 3, 0);
+                        ElementTransformUtils.MoveElement(doc, eleId, moveVec);
+
+                        //Rotate element
+                        LocationPoint location = ele.Location as LocationPoint;
+                        XYZ p1 = location.Point;
+                        XYZ p2 = new XYZ(p1.X, p1.Y, p1.Z + 10);
+                        Line axis = Line.CreateBound(p1, p2);
+                        double angle = 30 * Math.PI / 180;
+
+                        ElementTransformUtils.RotateElement(doc, eleId, axis, angle);
+
+                        trans.Commit();
+                    }
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
