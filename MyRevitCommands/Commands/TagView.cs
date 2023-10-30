af36ecec9c8f3f88e6c9a85ada02933cfc4b921b
frankleyyago <frankleyyago@outlook.com>
2023-10-30 17:38:21 -0300

    feat: tag all window and door

---
+++ MyRevitCommands/Commands/TagView.cs
@@ -0,0 +1,65 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRevitCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class TagView : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get document
+            Document doc = uidoc.Document;
+
+            //Tag parameters
+            TagMode tmode = TagMode.TM_ADDBY_CATEGORY;
+            TagOrientation tOrient = TagOrientation.Horizontal;
+
+            List<BuiltInCategory> cats = new List<BuiltInCategory>();
+            cats.Add(BuiltInCategory.OST_Windows);
+            cats.Add(BuiltInCategory.OST_Doors);
+
+            ElementMulticategoryFilter filter = new ElementMulticategoryFilter(cats);
+
+            IList<Element> tElements = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .WherePasses(filter)
+                .WhereElementIsNotElementType()
+                .ToElements();
+
+            try
+            {
+                using (Transaction trans = new Transaction(doc, "Tag Elements"))
+                {
+                    trans.Start();
+
+                    //Tag elements
+                    foreach (Element ele in tElements)
+                    {
+                        Reference refe = new Reference(ele);
+                        LocationPoint loc = ele.Location as LocationPoint;
+                        XYZ point = loc.Point;
+                        IndependentTag tag = IndependentTag.Create(doc, doc.ActiveView.Id, refe, true, tmode, tOrient, point);
+                    }
+
+                    trans.Commit();
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
