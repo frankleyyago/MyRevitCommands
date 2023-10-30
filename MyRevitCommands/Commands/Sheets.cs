c8097c213e98862592d393814c4aa69b9ca7f138
frankleyyago <frankleyyago@outlook.com>
2023-10-30 18:44:31 -0300

    feat: create a sheet

---
+++ MyRevitCommands/Commands/Sheets.cs
@@ -0,0 +1,54 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRevitCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class Sheets : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get document
+            Document doc = uidoc.Document;
+
+            //Get family symbol
+            FamilySymbol tBlock = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                .WhereElementIsElementType()
+                .Cast<FamilySymbol>()
+                .First();
+
+            try
+            {
+                using (Transaction trans = new Transaction(doc, "Create sheet"))
+                {
+                    trans.Start();
+
+                    //Create sheet
+                    ViewSheet vSheet = ViewSheet.Create(doc, tBlock.Id);
+                    vSheet.Name = "My first sheet";
+                    vSheet.SheetNumber = "J101";
+
+                    trans.Commit();
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
